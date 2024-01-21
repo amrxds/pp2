6fe339f21a6f0e636f1cc73aff7661e069d75801
amrxds <a_dossymov@kbtu.kz>
2024-01-21 16:13:11 +0600

    first commit

---
+++ lab1/variables/names/example2.py
@@ -0,0 +1,18 @@
+#Camel Case
+#Each word, except the first, starts with a capital letter:
+
+myVariableName = "John"
+#Pascal Case
+#Each word starts with a capital letter:
+
+MyVariableName = "John"
+
+#Snake Case
+#Each word is separated by an underscore character:
+
+my_variable_name = "John"
+
+
+print(myVariableName)
+print(MyVariableName)
+print(my_variable_name)
